9e64262942937ddca5ea967c55a615b5e0c74cd1
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-16 23:12:05 +0530

    fix : Modify updated_variables.dart.

---
+++ lib/updated_variables.dart
@@ -0,0 +1,17 @@
+
+class UpdatedVariables{
+
+  UpdatedVariables(this.weight,this.height);
+  final double weight;
+  final double height;
+
+  double returnWeight(){
+    double _weight=weight;
+    return _weight;
+  }
+
+  double returnHeight(){
+    double _height=height;
+    return _height;
+  }
+}
