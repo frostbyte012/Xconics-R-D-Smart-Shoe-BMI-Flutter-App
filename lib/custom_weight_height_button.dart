c6a04906b687e3642f8cd68739904310a287f694
Deepraj Majumdar <murieldeepraj@gmail.com>
2022-06-16 23:05:30 +0530

    fix : Modify custom_height_button.dart

--- lib/custom_weight_height_button.dart
+++ lib/custom_weight_height_button.dart
@@ -1,59 +1,81 @@
 import 'package:flutter/material.dart';
 import 'constants.dart';
+import 'dart:io';
+import 'dart:math';
 import 'rounded_custom_button.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 
-class WeightAgeStateLessWidget extends StatelessWidget {
-  const WeightAgeStateLessWidget({
-    required this.title,
-  });
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-
-          Text(title,style: kLabelTextStyle,),
-          WeightAgeStateFulWidget(),
-
-        ]
-    );
-  }
-}
-
-
 class WeightAgeStateFulWidget extends StatefulWidget {
 
-  @override
+  WeightAgeStateFulWidget({
+
+    required this.title,
+
+});
+
+  final String title;
+
+
+
   State<WeightAgeStateFulWidget> createState() => _WeightAgeStateFulWidgetState();
 }
 
 class _WeightAgeStateFulWidgetState extends State<WeightAgeStateFulWidget> {
 
-  double weight_age_var=0.0;
+  int weight_age_var=25;
+
+  final filename='lib/weight.txt';
+
+  void writeWeight () async {
+    File(filename).writeAsString(weight_age_var.toString());
+  }
+
+  double roundDouble(double value, int places){
+    num mod = pow(10.0, places);
+    return ((value * mod).round().toDouble() / mod);
+  }
 
 
-  void subtractWeight()
+  void subtractWeight() async
   {
     setState((){
       if(weight_age_var>0)
+
         {
-          weight_age_var--;
+          if(widget.title=='WEIGHT')
+          {
+            weight_age_var-=1;
+            writeWeight();
+          }
+
+          else{
+            weight_age_var--;
+          }
+
         }
+
     });
   }
 
-  void addWeight()
+
+  void addWeight() async
   {
     setState((){
       if(weight_age_var<200)
-      {
-        weight_age_var++;
-      }
+        {
+          if(widget.title=='WEIGHT')
+          {
+            weight_age_var+=1;
+            writeWeight();
+          }
+
+          else{
+            weight_age_var++;
+          }
+
+        }
+
     });
   }
 
@@ -65,6 +87,7 @@ class _WeightAgeStateFulWidgetState extends State<WeightAgeStateFulWidget> {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center ,
       children: [
+        Text(widget.title,style: kLabelTextStyle,),
         Text(weight_age_var.toString(),style: kNumberTextStyle,),
         Row(
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -78,7 +101,7 @@ class _WeightAgeStateFulWidgetState extends State<WeightAgeStateFulWidget> {
         ),
 
       ],
-    );;
+    );
   }
 }
 
